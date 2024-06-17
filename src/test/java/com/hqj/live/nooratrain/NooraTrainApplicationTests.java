5d26d10d6ba094ebfea823ddec4e6f63abe83c62
hqj <1571149173@qq.com>
2024-06-18 00:27:12 +0800

    first commit

---
+++ src/test/java/com/hqj/live/nooratrain/NooraTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqj.live.nooratrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NooraTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
