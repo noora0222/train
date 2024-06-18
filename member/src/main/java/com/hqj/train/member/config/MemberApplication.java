af79781ecd128d71d1f75e924d4a295b3cc65179
huqijian <1571149173@qq.com>
2024-06-18 14:56:20 +0800

    complete 4.7 增加LogAspect,打印日志

--- member/src/main/java/com/hqj/train/member/config/MemberApplication.java
+++ member/src/main/java/com/hqj/train/member/config/MemberApplication.java
@@ -19,6 +19,5 @@ public class MemberApplication {
         LOG.info("启动成功");
         //LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
         LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
-
     }
 }
